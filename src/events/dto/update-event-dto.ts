5ff3e5f43f31c9965a7e8e66263fdc71edd0de30
Hugo Jeller <hugo_jeller@hotmail.com>
2021-07-30 10:10:26 -0400

    Fixes missing title in UpdateEvent DTO

--- src/events/dto/update-event-dto.ts
+++ src/events/dto/update-event-dto.ts
@@ -2,6 +2,9 @@ import {IsOptional} from 'class-validator';
 
 export class UpdateEventDto {
     @IsOptional()
+    title: string;
+    
+    @IsOptional()
     description: string;
 
     @IsOptional()
