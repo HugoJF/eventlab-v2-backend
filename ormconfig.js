ae84822fc13b41410a004a3549499fb21e3aa209
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2021-08-04 12:29:25 -0400

    Replace serverless-express package to a more updated one

--- ormconfig.js
+++ ormconfig.js
@@ -7,6 +7,7 @@ module.exports = {
     "database": process.env.DB_DATABASE || "eventlab",
     "entities": [
         "./dist/**/*.entity.ts", "./dist/**/*.entity.js",
+        "./.build/**/*.entity.js",
         "./src/**/*.entity.js",
     ],
     "synchronize": process.env.DB_SYNCHRONIZE || true,
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,35 +1,32 @@
-// lambda.ts
-import { Handler, Context } from 'aws-lambda';
-import { Server } from 'http';
-import { createServer, proxy } from 'aws-serverless-express';
-import { eventContext } from 'aws-serverless-express/middleware';
-
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import serverlessExpress from '@vendia/serverless-express';
+import { Context, Handler } from 'aws-lambda';
+import express from 'express';
+
 import { AppModule } from './app.module';
 
-const express = require('express');
+let cachedServer: Handler;
 
-// NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
-// due to a compressed response (e.g. gzip) which has not been handled correctly
-// by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
-// binaryMimeTypes below
-const binaryMimeTypes: string[] = [];
-
-let cachedServer: Server;
-
-async function bootstrapServer(): Promise<Server> {
+async function bootstrap() {
     if (!cachedServer) {
         const expressApp = express();
-        const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp))
-        nestApp.use(eventContext());
+        const nestApp = await NestFactory.create(
+            AppModule,
+            new ExpressAdapter(expressApp),
+        );
+
+        nestApp.enableCors();
+
         await nestApp.init();
-        cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+
+        cachedServer = serverlessExpress({ app: expressApp });
     }
+
     return cachedServer;
 }
 
-export const handler: Handler = async (event: any, context: Context) => {
-    cachedServer = await bootstrapServer();
-    return proxy(cachedServer, event, context, 'PROMISE').promise;
-}
+export const handler = async (event: any, context: Context, callback: any) => {
+    const server = await bootstrap();
+    return server(event, context, callback);
+};
